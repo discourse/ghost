0c2b5b68befa0777364ad92b98b09ca4f007c78c
awesomerobot <kris.aubuchon@discourse.org>
2025-02-17 18:00:42 -0500

    REFACTOR: update, remove template overrides, restyle

--- javascripts/discourse/api-initializers/init-ghost.js
+++ javascripts/discourse/api-initializers/init-ghost.js
@@ -1,21 +1,6 @@
-import $ from "jquery";
 import { apiInitializer } from "discourse/lib/api";
-import { withSilencedDeprecations } from "discourse/lib/deprecated";
 
 export default apiInitializer("0.8.0", (api) => {
-  withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
-    api.modifyClass("component:topic-list", {
-      pluginId: "ghost",
-      tagName: "ul",
-      classNames: ["ghost-legacy-topic-list"],
-    });
-
-    api.modifyClass("component:topic-list-item", {
-      pluginId: "ghost",
-      tagName: "li",
-    });
-  });
-
   api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
     columns.delete("views");
     columns.delete("likes");
@@ -23,34 +8,4 @@ export default apiInitializer("0.8.0", (api) => {
     columns.delete("replies");
     return columns;
   });
-
-  if (api.container.lookup("service:site").mobileView) {
-    api.modifyClass("controller:admin-site-settings", {
-      pluginId: "ghost",
-      actions: {
-        toggleMenu() {
-          $(".admin-nav").toggleClass("mobile-open");
-          $(".admin-detail").toggleClass("mobile-closed mobile-open");
-        },
-      },
-    });
-    api.modifyClass("controller:admin-plugins", {
-      pluginId: "ghost",
-      actions: {
-        toggleMenu() {
-          $(".admin-nav").toggleClass("mobile-open");
-          $(".admin-detail").toggleClass("mobile-closed mobile-open");
-        },
-      },
-    });
-    api.modifyClass("controller:admin-watched-words", {
-      pluginId: "ghost",
-      actions: {
-        toggleMenu() {
-          $(".admin-nav").toggleClass("mobile-open");
-          $(".admin-detail").toggleClass("mobile-closed mobile-open");
-        },
-      },
-    });
-  }
 });
